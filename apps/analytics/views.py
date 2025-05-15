5a6da276df7f5e64096ac40af7ef87c01e3c2309
BlackMamba51 <shvyryov200@gmail.com>
2025-05-15 09:31:46 +0300

    add a table with data on 100 cryptocurrencies, customized the display of numbers in the data

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -6,11 +6,12 @@ import requests
 
 @api_view(['GET']) # Разрешаем только GET запросы
 def get_crypto_data(request):
-
     return render(request, 'analytics/index.html')
 
+
+
 def fetch_crypto_api(request):
-    url = 'https://api.coinlore.net/api/ticker/?id=90'
+    url = 'https://api.coinlore.net/api/tickers/'
     response = requests.get(url)
     if response.status_code != 200:
         return JsonResponse({"error": "Ошибка при получении данных"}, safe=False)
